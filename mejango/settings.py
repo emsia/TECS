f6e02b22c3f910a57209ad01ac02c790b6f341e7
emsia <esia.rizal@gmail.com>
2013-09-30 10:23:33 +0800

    database

--- mejango/settings.py
+++ mejango/settings.py
@@ -12,7 +12,17 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+		'NAME': 'aeg',					  # Or path to database file if using sqlite3.
+		# The following settings are not used with sqlite3:
+		'USER': 'postgres',
+		'PASSWORD': '',
+		'HOST': '',					  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+		'PORT': '',					  # Set to empty string for default.
+	}
+}
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
