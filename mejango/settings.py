00b2220d26040fdf4125107206d367068291f2d2
emsia <esia.rizal@gmail.com>
2013-09-30 10:30:43 +0800

    database

--- mejango/settings.py
+++ mejango/settings.py
@@ -15,15 +15,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-		'NAME': 'aeg',					  # Or path to database file if using sqlite3.
-		# The following settings are not used with sqlite3:
-		'USER': 'postgres',
-		'PASSWORD': '',
-		'HOST': 'localhost',					  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-		'PORT': '5432',					  # Set to empty string for default.
-	}
+	'default': dj_database_url.config()
 }
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Hosts/domain names that are valid for this site; required if DEBUG is False
