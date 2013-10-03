3409b90f3a84c946ed24e3b651b544a35f7a5afb
emsia <esia.rizal@gmail.com>
2013-10-03 17:22:19 +0800

    reg

--- app_auth/views.py
+++ app_auth/views.py
@@ -90,26 +90,24 @@ def user_logout(request):
 @login_required(redirect_field_name='', login_url='/')
 def profile_view(request):
 	avatar = UserProfile.objects.get(user_id=request.user.id).avatar
-	role = UserProfile.objects.get(user_id = request.user.id).role
+
 	profile = UserProfile.objects.get(user=request.user)
 	if len(Teacher.objects.filter(user_id = request.user.id)) > 0:
 		role = 'Teacher'
 	elif len(Student.objects.filter(user_id = request.user.id)) > 0:
 		role = 'Student'
-	return render(request, 'app_auth/profile_view.html', {'avatar': avatar, 'role':role, 'profile':profile, 'role':role})
+	return render(request, 'app_auth/profile_view.html', {'avatar': avatar, 'role':role, 'profile':profile} )
 
 
 @login_required(redirect_field_name='', login_url='/')
 def profile_edit(request, success=None):
 	user_info = UserProfile.objects.filter(user_id = request.user.id)
-	power = False
 
 	if request.method == "POST":
 		formProfile = ProfileForm(request.POST, request.FILES)
 		schoolForm = schoolForStudent(request.POST)
 		if request.user.is_staff:
 			schoolForm = schoolForTeacher(request.POST)
-		power = True
 
 		if schoolForm.is_valid():
 			if request.user.is_staff:
@@ -158,42 +156,33 @@ def profile_edit(request, success=None):
 			USER_info.username = temp['username']
 			USER_info.save()
 			return redirect("auth:profile")
-			
+
 	if user_info.exists():
-		schoolForm = schoolForStudent()
 		user_info = user_info.get(user_id=request.user.id)
-		role = UserProfile.objects.get(user_id = request.user.id).role
 
 		if request.user.is_staff:
 			schoolForm = schoolForTeacher(initial={'school':Teacher.objects.get(user=request.user).school.values_list('id',flat=True)})
 		
 		avatar = user_info.avatar
-		if not power:
-			try:
-				schoolForm = schoolForStudent(initial={'school':Student.objects.get(user=request.user).school})
-			except:
-				pass
-			if request.user.is_staff:
-				schoolForm = schoolForTeacher(initial={'school':Teacher.objects.get(user=request.user).school.values_list('id',flat=True)})
-			formProfile = ProfileForm(initial={
-				'last_name':request.user.last_name, 'first_name':request.user.first_name, 'email':request.user.email, 'avatar':user_info.avatar,
-				'username': request.user.username, 'street':user_info.street, 'municipality':user_info.municipality,
-				'province': user_info.province, 'phone_number': user_info.phone_number
-			})
+		schoolForm = schoolForStudent(initial={'school':Student.objects.get(user=request.user).school})
+		formProfile = ProfileForm(initial={
+			'last_name':request.user.last_name, 'first_name':request.user.first_name, 'email':request.user.email, 'avatar':user_info.avatar,
+			'username': request.user.username, 'street':user_info.street, 'municipality':user_info.municipality,
+			'province': user_info.province, 'phone_number': user_info.phone_number
+		})
 	else:
 		schoolForm = schoolForStudent()
 		if request.user.is_staff:
 			schoolForm = schoolForTeacher()
 		avatar = 'images/avatars/user.png'
-		role = None
+
 		formProfile = ProfileForm(initial={'last_name':request.user.last_name, 'first_name':request.user.first_name, 'email':request.user.email,
 			'username': request.user.username,})
-	return render(request, 'app_auth/profile_edit.html', {'avatar': avatar, 'role':role, 'role':role, 'active_nav':'PROFILE', 'success':success, 'formProfile':formProfile, 'schoolForm':schoolForm})
+	return render(request, 'app_auth/profile_edit.html', {'avatar': avatar, 'active_nav':'PROFILE', 'success':success, 'formProfile':formProfile, 'schoolForm':schoolForm})
 
 @login_required(redirect_field_name='', login_url='/')
 def password_edit(request, success=None):
 	user_info = UserProfile.objects.filter(user_id = request.user.id)
-	role = UserProfile.objects.get(user_id = request.user.id).role
 
 	if not user_info.exists():
 		return redirect("/profile")
@@ -221,17 +210,16 @@ def password_edit(request, success=None):
 	else:
 		form_class = PasswordForm()
 
-	return render(request, 'app_auth/changePassword.html', {'avatar': avatar, 'role':role, 'active_nav':'ACCOUNT', 'power':power,'user_info':user_info, 'form':form_class, 'error': err, 'success':success})
+	return render(request, 'app_auth/changePassword.html', {'avatar': avatar, 'active_nav':'ACCOUNT', 'power':power,'user_info':user_info, 'form':form_class, 'error': err, 'success':success})
 
 @login_required(redirect_field_name='', login_url='/')
 def grading_system(request):
 	avatar = UserProfile.objects.get(user_id=request.user.id).avatar
-	role = UserProfile.objects.get(user_id = request.user.id).role
 
 	gradingsys = GradingSystem.objects.filter(created_by=request.user, is_active=1)
 	gradingsys_admin = GradingSystem.objects.filter(created_by=1, is_active=1)
 
-	return render(request, 'app_auth/grading_systems.html', {'avatar': avatar, 'role':role, 'active_nav':'GRADESYS', 'gradingsys':gradingsys, 'gradingsys_admin':gradingsys_admin})
+	return render(request, 'app_auth/grading_systems.html', {'avatar': avatar, 'active_nav':'GRADESYS', 'gradingsys':gradingsys, 'gradingsys_admin':gradingsys_admin})
 
 @login_required(redirect_field_name='', login_url='/')
 def grading_system_view(request, gradeSys_id):
@@ -243,7 +231,6 @@ def grading_system_view(request, gradeSys_id):
 @login_required(redirect_field_name='', login_url='/')
 def grading_system_new(request):
 	avatar = UserProfile.objects.get(user_id=request.user.id).avatar
-	role = UserProfile.objects.get(user_id = request.user.id).role
 	
 	if request.method == 'POST':
 		formSys = GradeSysForm(request.POST, request)
@@ -290,7 +277,7 @@ def grading_system_new(request):
 		formGrade1 = GradeForm_Option1()
 		formGrade2 = GradeForm_Option2()
 
-	return render(request, 'app_auth/grading_systems_new.html', {'avatar': avatar, 'role':role, 'active_nav':'GRADESYS', 'formSys':formSys, 'formGrade1':formGrade1, 'formGrade2':formGrade2})
+	return render(request, 'app_auth/grading_systems_new.html', {'avatar': avatar, 'active_nav':'GRADESYS', 'formSys':formSys, 'formGrade1':formGrade1, 'formGrade2':formGrade2})
 
 
 @login_required(redirect_field_name='', login_url='/')
@@ -311,8 +298,6 @@ def dashboard(request):
 
 	if not User_Profile.exists():
 		return redirect('auth:edit_profile')
-	else:
-		role = User_Profile.get(user_id=request.user.id).role
 
 	avatar = User_Profile.get(user_id=request.user.id).avatar
 
@@ -320,12 +305,12 @@ def dashboard(request):
 		class_count = Class.objects.filter(teacher=Teacher.objects.get(user=request.user), is_active=1).count()
 		exam_count = Essay.objects.filter(instructor = Teacher.objects.get(user = request.user), status=1).filter(start_date__lte=timezone.now(), deadline__gte=timezone.now()).count()
 		needs_grading_count =   EssayResponse.objects.filter(essay=Essay.objects.filter(instructor_id = Teacher.objects.get(user_id = request.user.id).id).filter(deadline__lt=timezone.now()), grade=None).count()
-		return render(request, 'app_auth/teacher_dashboard.html', {'avatar': avatar, 'role':role, 'class_count':class_count, 'exam_count':exam_count, 'needs_grading_count':needs_grading_count})
+		return render(request, 'app_auth/teacher_dashboard.html', {'avatar': avatar, 'class_count':class_count, 'exam_count':exam_count, 'needs_grading_count':needs_grading_count})
 	elif len(Student.objects.filter(user_id = request.user.id)) > 0:
 		class_count = Class.objects.filter(student=Student.objects.get(user=request.user), is_active=1).count()
 		exam_count = EssayResponse.objects.filter(~Q(essay__status=0), student=Student.objects.get(user_id = request.user.id)).filter(essay__start_date__lte=timezone.now(), essay__deadline__gte=timezone.now()).count()
 		in_progress_count = EssayResponse.objects.filter(~Q(essay__status=1), student=Student.objects.get(user_id = request.user.id)).filter(essay__start_date__lte=timezone.now(), essay__deadline__gte=timezone.now()).count()
-		return render(request, 'app_auth/student_dashboard.html', {'avatar': avatar, 'role':role, 'class_count':class_count, 'exam_count':exam_count, 'in_progress_count':in_progress_count})
+		return render(request, 'app_auth/student_dashboard.html', {'avatar': avatar, 'class_count':class_count, 'exam_count':exam_count, 'in_progress_count':in_progress_count})
 
 @login_required(redirect_field_name='', login_url='/')
 def help(request):
