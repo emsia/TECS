a65fd1b5cdc822e983d5a323ae6ece0e4b9db413
emsia <esia.rizal@gmail.com>
2014-03-27 11:46:58 +0800

    init.R and libraries needed

--- init.r
+++ init.r
@@ -1,2 +1,4 @@
-install.packages(c("MASS", "tm", "Rstem"), dependencies=TRUE)
-install.packages("./library/lsa_0.57.tar.gz", repos=NULL, type="source")
+install.packages(c("MASS", "tm", "RWeka"), dependencies=TRUE)
+install.packages("Rstem", repos = "http://www.omegahat.org/R")
+install.packages("./library/Snowball_0.0-11.tar.gz", repos=NULL)
+install.packages("./library/lsa_0.63-3.tar.gz", repos=NULL)
